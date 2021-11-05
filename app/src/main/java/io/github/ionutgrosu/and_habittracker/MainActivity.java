111431aade83de89c7678b669b2c4c6959d354eb
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2021-11-05 14:03:00 +0100

    Added: * overview fragment * account fragment * friends fragment * create fragment * navigation component

--- app/src/main/java/io/github/ionutgrosu/and_habittracker/MainActivity.java
+++ app/src/main/java/io/github/ionutgrosu/and_habittracker/MainActivity.java
@@ -1,14 +1,63 @@
 package io.github.ionutgrosu.and_habittracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FloatingActionButton fab;
+    private MaterialToolbar topbar;
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setupNavigation();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.createFragment);
+            }
+        });
+    }
+
+    private void setupNavigation() {
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(3).setEnabled(false);
+
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        navController = navHostFragment.getNavController();
+
+    }
+
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fab = findViewById(R.id.fab);
+        topbar = findViewById(R.id.topAppBar);
+    }
+
+    public void setTopbarTitle(String title){
+        topbar.setTitle(title);
     }
 }
---
+++ app/src/main/java/io/github/ionutgrosu/and_habittracker/OverviewFragment.java
@@ -0,0 +1,31 @@
+package io.github.ionutgrosu.and_habittracker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OverviewFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MainActivity) getActivity()).setTopbarTitle("Overview");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+
+    }
+}

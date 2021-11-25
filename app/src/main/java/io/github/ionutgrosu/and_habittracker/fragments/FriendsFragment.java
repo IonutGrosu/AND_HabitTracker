39c3441aa769db8a191cb4c67bb57369aaaa5fdf
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2021-11-25 18:13:40 +0100

    Added friend fragment layout

--- app/src/main/java/io/github/ionutgrosu/and_habittracker/fragments/FriendsFragment.java
+++ app/src/main/java/io/github/ionutgrosu/and_habittracker/fragments/FriendsFragment.java
@@ -7,21 +7,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import io.github.ionutgrosu.and_habittracker.activities.MainActivity;
 import io.github.ionutgrosu.and_habittracker.R;
 
 public class FriendsFragment extends Fragment {
 
+    private TextInputEditText friendRequestInput;
+    private Button friendRequestBtn;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((MainActivity) getActivity()).setTopbarTitle("Friends");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        initViews(view);
+
+        return view;
+    }
+
+    private void initViews(View view) {
+        ((MainActivity) getActivity()).setTopbarTitle("Friends");
+
+        friendRequestInput = view.findViewById(R.id.friendRequestInput);
+        friendRequestBtn = view.findViewById(R.id.friendRequestBtn);
     }
 }
